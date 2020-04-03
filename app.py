6fb45db84a373872eb0f87b99b8ccd00cc04ba5a
Svitlana <sveta_naz@yahoo.com>
2020-04-03 10:15:13 -0400

    Second commit no data

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ import json
 app = Flask(__name__)
 
 
+globalData=[]
+nycData_jan=[]
+nycData_apr=[]
+nycData_july=[]
+nycData_oct=[]
+nycData_feb=[]
 
 # Flask Routes
 @app.route('/')
@@ -17,9 +23,7 @@ def index():
 def home():
     return render_template('index.html')
 
-@app.route('/stations')
-def stations():
-    return render_template('stations.html')
+
 
 @app.route('/stations1')
 def stations1():
@@ -53,107 +57,139 @@ def add_header(r):
     r.headers["Expires"] = "0"
     return r
 
+
+
 #To get data from MongoDB
-@app.route('/stationlocations')
-def stationlocation():
+@app.before_first_request
+def loadGlobal():
     conn='mongodb://localhost:27017'
     client = MongoClient(conn)
     db=client.bikeshareDB
     stations = db.bike_companies.find()
 
-    
+    #global data load 
     data=[]
+    global globalData
     
     for station in stations:
         
         data.append({'id': station['id'],'name': station['name'], 'country': station['location']['country'], 'city': station['location']['city'],
         'lat': station['location']['latitude'], 'lng': station['location']['longitude'], 'stations':station['stations']})
         
-    return {'companies':data}
+    globalData=data
 
-@app.route('/nyc_data_july')
-def july_nycdata():
-    
-    conn='mongodb://localhost:27017'
-    client = MongoClient(conn)
-    db=client.bikeshareDB
-    trips=[]
-    
-    trips = db.nyc_bikes.find()
-    
-    data=[]
-    i=0
-    for trip in trips:    
-        if (i>100000): 
-            break
-        data.append({'duration':trip['tripduration'], 'gender':trip['gender'], 'birth_year':trip['birth year']})  
-        i=i+1
-    
-    
-    return {'trips':data} 
-
-@app.route('/nyc_data_oct')
-def oct_nycdata():
-    
-    conn='mongodb://localhost:27017'
-    client = MongoClient(conn)
-    db=client.bikeshareDB
-    trips=[]
-    
-    trips = db.nyc_bikes_oct.find()
-    
-    data=[]
-    i=0
-    for trip in trips:    
-        if (i>100000): 
-            break
-        data.append({'duration':trip['tripduration'], 'gender':trip['gender'], 'birth_year':trip['birth year']})  
-        i=i+1
-    
-    
-    return {'trips':data} 
-
-@app.route('/nyc_data_jan')
-def jan_nycdata():
-    
-    conn='mongodb://localhost:27017'
-    client = MongoClient(conn)
-    db=client.bikeshareDB
-    trips=[]
-    
+    #January data load
     trips = db.nyc_bikes_jan.find()
     
     data=[]
     i=0
+    global nycData_jan
     for trip in trips:    
-        if (i>100000): 
+        if (i>=100000): 
+            break
+        data.append({'duration':trip['tripduration'], 'gender':trip['gender'], 'birth_year':trip['birth year']})  
+        i=i+1
+
+    nycData_jan=data
+
+
+    #February data load
+    trips = db.nyc_bikes_feb.find()
+    
+    data=[]
+    i=0
+    global nycData_feb
+
+    for trip in trips:    
+        if (i>=100000): 
             break
         data.append({'duration':trip['tripduration'], 'gender':trip['gender'], 'birth_year':trip['birth year']})  
         i=i+1
     
-    
-    return {'trips':data} 
+    nycData_feb=data
 
-@app.route('/nyc_data_apr')
-def apr_nycdata():
-    
-    conn='mongodb://localhost:27017'
-    client = MongoClient(conn)
-    db=client.bikeshareDB
-    trips=[]
-    
+    #April data load
     trips = db.nyc_bikes_apr.find()
     
     data=[]
     i=0
+    global nycData_apr
+
     for trip in trips:    
-        if (i>100000): 
+        if (i>=100000): 
             break
         data.append({'duration':trip['tripduration'], 'gender':trip['gender'], 'birth_year':trip['birth year']})  
         i=i+1
     
+    nycData_apr=data
+
+    #July data load
+    trips = db.nyc_bikes.find()
+    data=[]
+    i=0
+    global nycData_july
+
+    for trip in trips:    
+        if (i>=100000): 
+            break
+        data.append({'duration':trip['tripduration'], 'gender':trip['gender'], 'birth_year':trip['birth year']})  
+        i=i+1
+
+    nycData_july=data
     
-    return {'trips':data} 
+    #October data load
+    trips = db.nyc_bikes_oct.find()
+    
+    data=[]
+    i=0
+    global nycData_oct
+
+    for trip in trips:    
+        if (i>=100000): 
+            break
+        data.append({'duration':trip['tripduration'], 'gender':trip['gender'], 'birth_year':trip['birth year']})  
+        i=i+1
+
+    nycData_oct=data
+
+    
+
+@app.route('/stationlocations') 
+def stationlocation():
+    return {'companies':globalData}
+
+@app.route("/global_list")
+def global_list_filter():
+    return render_template("global_list.html")
+
+
+
+''' @app.route("/get_ny_list_db/<start_date>/<max_results>") # not user url
+def get_ny_list_db(start_date, max_results):
+    print("Fetching data ... ")
+    db_conn_string = 'mongodb://localhost:27017'
+    return get_ny_list_mongodb(db_conn_string, start_date, int(max_results)) '''
+
+@app.route('/nyc_data_jan')
+def jan_nycdata():
+    return {'trips':nycData_jan}
+
+@app.route('/nyc_data_feb')
+def feb_nycdata():
+    return {'trips':nycData_feb} 
+
+@app.route('/nyc_data_apr')
+def apr_nycdata():
+    return {'trips':nycData_apr} 
+
+@app.route('/nyc_data_july')
+def july_nycdata():
+    return {'trips':nycData_july} 
+
+@app.route('/nyc_data_oct')
+def oct_nycdata():
+    return {'trips':nycData_oct} 
+
 
 if __name__ == '__main__':
     app.run(debug=True)
